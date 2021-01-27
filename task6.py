5d3a5e22875b401ac22ea32ed9d5b44d95ac7e56
DmitryBBB <77457078+DmitryBBB@users.noreply.github.com>
2021-01-27 20:36:37 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,9 +1,27 @@
+print("Программа инвентаризации склада магазина")
+
+i = int(input("Введите количество наименований на складе магазина: "))
 products = []
-while True:
-    all_products = input("Сколько всего наименований продукции? ")
-    if not all_products.isdigit():
-        print("!!Введите число!!")
-    else:
-        all_products = int(all_products)
-        break
-print("Наименований", all_products)
+quantity = []
+unit = []
+price = []
+while len(products) != i:
+    products.append(input("Наименование товара: "))
+    quantity.append(input("Введите количество: "))
+    unit.append(input("Единица измерения: "))
+    price.append(input("Цена за единицу товара: "))
+print(price, products, quantity, unit)
+warehouse = {
+    "Наименование": products,
+    "Количество": quantity,
+    "Единица измерения": unit,
+    "Цена": price
+}
+print(warehouse)
+analitic = (
+    1, {"Наименование": products},
+    2, {"Количество": quantity},
+    3, {"Единица измерения": unit},
+    3, {"Цена": price}
+)
+print(analitic)
