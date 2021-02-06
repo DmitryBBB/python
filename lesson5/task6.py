ff42aae5b9cac0a624cc44d53b29ddf6cf470603
DmitryBBB <ark-bykov@yandex.ru>
2021-02-06 15:24:11 +0300

    Домашка 5-й урок

--- lesson5/task6.py
+++ lesson5/task6.py
@@ -12,7 +12,95 @@
 # Пример словаря:
 # {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
-with open('task6.txt', 'r', encoding='utf8') as f:
-    study_list = f.readlines()
-    print(study_list)
-    summ_time = {}
+
+with open('task6.txt', 'r', encoding='utf8') as file:
+    study = file.readlines()
+    lessons = {}
+    for line in study:
+        inf = line.replace('(', ' ').split()
+        lessons[inf[0][:-1]] = sum(int(i) for i in inf if i.isdigit())
+    print(f'Общее количество часов по предмету: \n{lessons}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def func(string):
+#    try:
+ #       numbers = ""
+ #       for i in string:
+ #           if i.isdigit() is True:
+ #               numbers += i
+ #       numbers = int(numbers)
+ #       return numbers
+#    except ValueError:
+ #       return 0
+
+
+#with open('task6.txt', encoding='utf8') as f:
+ #  y = f.read().split("\n")[-1]
+ #  print(y)
+ #  dict = dict()
+ #  for item in y:
+ #      key = item.split(" ")[0]
+ #      value = item.split(" ")[1:]
+ #      dict[key] = value
+
+#print(dict)
+
+
+
+
+# for line in f:
+     #   subject, lecture, practice, lab = line.split()
+      #  obj[subject] = func(lecture) + func(practice) + func(lab)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#my_dict = dict()
+#for item in study_list:
+    #key = item.split(" ")[0]
+    #value = item.split(" ")[1:]
+    #my_dict[key] = value
+#print(my_dict)
+
+
+
+
+
+
+
+
