04cf4f8778d2d83adbc79e0b71f414bcfd88430b
DmitryBBB <ark-bykov@yandex.ru>
2021-02-08 20:38:36 +0300

    Домашка 5-й урок

--- lesson5/task7.py
+++ lesson5/task7.py
@@ -17,19 +17,50 @@
 # [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 #
 # Подсказка: использовать менеджеры контекста.
-profit = dict()
-profit_summ = []
-profit_firm = []
-with open("task7.txt", "r") as company:
-    firm = company.read().split("\n")
-    for item in firm:
-        key = item.split(" ")[0]
-        value = item.split(" ")[1:]
-        profit[key] = value
-    profit.popitem()
-    print(profit)
+#profit = dict()
+#profit_summ = {}
+#profit_firm = []
+#with open("task7.txt", "r") as company:
+    #firm = company.read().split("\n")
+    #for item in firm:
+        #key = item.split(" ")[0]
+        #value = item.split(" ")[2:]
+        #profit[key] = value
+    #profit.popitem()
+    #print(profit)
+profit = {}
+profit_firms = []
+with open("task7.txt", "r", encoding="utf-8") as firms:
 
-print(firm)
+    while True:
+        line = firms.readline().split()
+        if not line:
+            break
+        profit[line[0]] = float(line[-2]) - float(line[-1])
+
+        if profit[line[0]] > 0:
+            profit_firms.append(profit[line[0]])
+        else:
+            unprofitable_firm = (profit[line[0]])
+            del profit[line[0]]
+
+    print(profit_firms)
+    print(unprofitable_firm)
+result = [profit, {"average_profit": sum(profit_firms) / len(profit_firms)}, {"unprofitable_firm": unprofitable_firm}]
+print(result)
+
+import json
+with open("file.json", 'w', encoding='utf-8') as file_j:
+    json.dump(result, file_j)
+
+
+
+
+
+
+
+
+
 
 
 
